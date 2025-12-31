a8b68655965696a113294db7b80680f0d80b26cd
leisingha <120764124+leisingha@users.noreply.github.com>
2025-12-31 15:49:12 -0500

    main api fetch logic

---
+++ Plan.md
@@ -0,0 +1,1 @@
+Simple Weather App with a card showing : day/night graphics, temperature, location, time, Forecast days, Weather Alerts, Search API and Weather Maps.
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import "./style.css";
 
-async function getWeatherData() {
-  const response = fetch();
+async function getWeatherData(location) {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=fa016dd8972641d5b3d33653253112&q=${location}&aqi=yes`
+    );
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    const temp = data.current.temp_c;
+    const feelslike = data.current.feelslike_c;
+    const weatherIcon = data.current.condition.icon;
+    const weatherDesc = data.current.condition.text;
+    console.log(temp, feelslike, weatherDesc);
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+getWeatherData("Toronto");
