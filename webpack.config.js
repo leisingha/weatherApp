6862b6c71ba1a1ea106265422ef73a83574f5e31
leisingha <120764124+leisingha@users.noreply.github.com>
2026-01-01 13:40:06 -0500

    update configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/script.js",
   output: {
-    filename: "main.js",
+    filename: "script.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
